9849761ae75caa3cf65c62424b1c97bd9a5cb1f1
Ryan Spencer <ryan.spencer1220@gmail.com>
2022-08-12 13:02:30 -0700

    develop initial functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+import Header from "./components/Header";
+import { useState } from "react";
 
 function App() {
+  const [url, setURL] = useState("");
+  const [shortURL, setShortURL] = useState("");
+
+  const handleChange = (e) => {
+    setURL(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(url);
+    const axios = require("axios");
+
+    const headers = {
+      "Content-Type": "application/json",
+      apikey: "533f3dcc3fad462196e928a29d98e4a8",
+    };
+
+    const shorten = async (url) => {
+      let endpoint = "https://api.rebrandly.com/v1/links";
+      let linkRequest = {
+        destination: url,
+      };
+      const apiCall = {
+        method: "post",
+        url: endpoint,
+        data: linkRequest,
+        headers: headers,
+      };
+      let apiResponse = await axios(apiCall);
+      let link = apiResponse.data;
+      return link.shortUrl;
+    };
+    let shortUrl = await shorten(url);
+    setShortURL(shortUrl);
+  };
+
+  const handleClick = () => {
+    console.log(shortURL);
+    window.open(`https://www.${shortURL}`);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />;
+      <form
+        className="mx-20 p-20 card bg-slate-100 flex flex-row"
+        onSubmit={handleSubmit}
+      >
+        <input
+          type="text"
+          placeholder="Enter URL here!"
+          className="input input-bordered input-primary input-lg mb-10 basis-10/12 mr-2"
+          value={url}
+          onChange={handleChange}
+        />
+        <button type="submit" className="btn basis-2/12 ml-2">
+          Submit
+        </button>
+      </form>
+      <div className="mx-20 p-20 card bg-slate-100 flex flex-row mt-10 text-xl">
+        Shortened URL: {shortURL}
+        {shortURL && (
+          <>
+            <button className="btn mx-4" onClick={handleClick}>
+              Try It Out!
+            </button>
+            <button className="btn" onClick={handleClick}>
+              Copy!
+            </button>
+          </>
+        )}
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="p-20 card bg-slate-100 mx-20 mt-20 mb-10 shadow-xl text-center text-7xl">
+      <h1>Link Shortener App</h1>
+    </div>
+  );
+};
+
+export default Header;
